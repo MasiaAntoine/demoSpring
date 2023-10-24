1710d17e0eefac1d90f266c60639de1a88066ca3
ThorKild_ <pro.antoine.masia@gmail.com>
2023-10-24 15:27:57 +0200

    v2

--- src/main/java/com/example/demoSpring/DemoSpringApplication.java
+++ src/main/java/com/example/demoSpring/DemoSpringApplication.java
@@ -2,14 +2,25 @@ package com.example.demoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableWebMvc
+@RestController
 public class DemoSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("==============================>  main");
+		System.out.println("==============================>  main");
+		System.out.println("==============================>  main");
+		System.out.println("==============================>  main");
 		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
