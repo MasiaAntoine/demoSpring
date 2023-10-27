2da8f90431973d67836b0dcc0f15d3d0eef39734
ThorKild_ <pro.antoine.masia@gmail.com>
2023-10-27 16:45:31 +0200

    mise a jour

--- src/main/java/com/example/demoSpring/controller/WizardController.java
+++ src/main/java/com/example/demoSpring/controller/WizardController.java
@@ -1,17 +1,18 @@
 package com.example.demoSpring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demoSpring.model.WizardModelWrapper;
 import com.example.demoSpring.service.WizardService;
 
-@RestController
+import lombok.RequiredArgsConstructor;
+
+@RestController("WizardController")
+@RequiredArgsConstructor
 public class WizardController {
 
-        @Autowired
-        private WizardService wizardService;
+        private final WizardService wizardService;
 
         @GetMapping("/wizard/spec")
         public WizardModelWrapper getWizardSpec() {
